7e8cc87996078bcfbc85a1d29544e8f5db8ec203
SofiaRomeiro <sofiaromeiro23@tecnico.ulisboa.pt>
2021-12-31 01:55:58 +0000

    Testing bugs + figured out the bug origin

--- tecnicofs/fs/operations.c
+++ tecnicofs/fs/operations.c
@@ -300,6 +300,79 @@ int tfs_handle_indirect_block(inode_t *inode, open_file_entry_t *file) {
 }
 
 ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
+    size_t size_read = 0;
+    
+    open_file_entry_t *file = get_open_file_entry(fhandle);
+    if (file == NULL) {
+        return -1;
+    }
+
+    inode_t *inode = inode_get(file->of_inumber);
+
+    printf("inode i size = %ld\n", inode->i_size);
+    
+    if (inode == NULL) {
+        return -1;
+    }
+
+    file->of_offset = 0;
+
+    for (int i = 0; size_read <= len && (i < 10); i++) {
+        void* block = data_block_get(inode->i_block[i]);
+
+        if (block == NULL) {
+            printf("[-] Error: NULL block\n");
+            return -1;
+        }
+
+        if (len - size_read > 1024) {
+            memcpy(buffer + size_read, block, 1024);
+            printf("FINALLLLLLLLLL ize_read: %ld\n", strlen(buffer));
+            size_read += 1024;
+        }
+
+        else {
+            memcpy(buffer + size_read, block, len - size_read);
+            printf("FINALLLLLLLLLL ize_read: %ld\n", strlen(buffer));
+            size_read += (len - size_read);
+        }
+        printf("size_read: %ld\n", size_read);
+    }
+
+    if ((len - size_read) > 0) {
+        int *block_from_i_block = (int *) data_block_get(inode->i_block[10]);
+
+        for (int i = 0; size_read <= len && (i < 256); i++) {
+            printf("i_block: block number %d\n", block_from_i_block[i]);
+            void* block = data_block_get(block_from_i_block[i]);
+            char* x_block = (char*) block;
+            printf("block: %ld\n", sizeof(x_block));
+
+            if (block == NULL) {
+                printf("[-] Error: NULL block_from_iblock\n");
+                return -1;
+            }
+
+            if (len - size_read > 1024) {
+                memcpy(buffer + size_read, block, 1024);
+                printf("FINALLLLLLLLLL ize_read: %ld\n", strlen(buffer));
+                size_read += 1024;
+            }
+
+            else {
+                memcpy(buffer + size_read, block, len - size_read);
+                printf("FINALLLLLLLLLL ize_read: %ld\n", strlen(buffer));
+                size_read += (len - size_read);
+            }
+            printf("size_read: %ld\n", size_read);
+        }
+    }
+    printf("FINALLLLLLLLLL ize_read: %ld\n", strlen(buffer));
+    return (ssize_t) size_read;
+}
+
+/*
+ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
 
     size_t to_read = 0;
     
@@ -309,7 +382,6 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
         return -1;
     }
 
-    /* From the open file table entry, we get the inode */
     inode_t *inode = inode_get(file->of_inumber);
 
     printf("inode i size = %ld\n", inode->i_size);
@@ -319,7 +391,6 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
     }
 
     if (file->of_offset < 1024) {
-        /* Determine how many bytes to read */
         to_read = inode->i_size - file->of_offset;
     }
 
@@ -340,11 +411,8 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
             return -1;
         }
 
-                /* Perform the actual read */
         memcpy(buffer, block + file->of_offset, to_read);
 
-        /* The offset associated with the file handle is
-         * incremented accordingly */
         file->of_offset += to_read;
 
     }
@@ -356,15 +424,14 @@ ssize_t tfs_read(int fhandle, void *buffer, size_t len) {
             return -1;
         }
 
-        /* Perform the actual read */
         memcpy(buffer, block + file->of_offset, to_read); //????? DEST SRC NBYTES
-        /* The offset associated with the file handle is
-         * incremented accordingly */
+
         file->of_offset += to_read;      
     }
 
     return (ssize_t)to_read;
 }
+*/
 
 int tfs_copy_to_external_fs(char const *source_path, char const *dest_path) {
 
